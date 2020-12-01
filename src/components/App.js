9a34b38c4506217b6a14254d072201b1b41c9e71
pratik kumar <kumarpratik861@gmail.com>
2020-12-01 23:55:18 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -155,7 +155,52 @@ const states = [
 ];
 
 function App() {
-  return <div id="main"></div>;
+  const getCities = (stateSelected) => {
+    let stateObj = states.filter((state) => state.name === stateSelected);
+    return stateObj[0].cities;
+  };
+
+  const getTowns = (cities, citySelected) => {
+    let cityObj = cities.filter((city) => city.name === citySelected);
+    return cityObj[0].towns;
+  };
+
+  let citiesList = getCities(states[0].name);
+  let townsList = getTowns(citiesList,citiesList[0].name);
+
+  const[selectedState,setSelectedState] =useState(States[0].name);
+  const[selectedCity, setSelectedCity] = useState(States[0].cities[0].name);
+  const[selectedCitiesList, setSelectedCitiesList] = useState(citiesList);
+  const][selectedTownsList, setSelectedTownsList] = useState(townsList);
+
+  const handleStatesDropdown =(event) => {
+    setSelectedState(event.target.value);
+    const newCitiesList = getCities(event.target.value);
+    setSelectedCitiesList(newCitiesList);
+    setSelectedCity(newCitiesList[0].name);
+    const newTownsList = getTowns(newCitiesList, newCitiesList[0].name);
+    setSelectedTownsList(newTownsList);
+
+  };
+
+  const handleCitiesDropdown = (event) => {
+    setSelectedCity(event.target.value);
+    const newTownsList = getTowns(selectedCitiesList,event.target.value);
+    setSelectedTownsList(newTownsList);
+  }
+  return <div id="main">
+    <States
+    states={states}
+    selectedState ={selectedState}
+    onChange = {(event) => handleStatesDropdown(event)}
+    />
+    <cities 
+    cities={selectedCitiesList}
+    selectedCity={selectedCity}
+    onChange= {(event) => handleCitiesDropdown(Eevnt)}
+    />
+    <Towns towns ={selectedTownsList}/>
+  </div>;
 }
 
 export default App;
